fd09232a5af68e510180446850ca809d25ecae77
Dominika <nika.dziubinska@gmail.com>
2018-09-09 23:55:32 +0200

    'buttony'

--- src/App.js
+++ src/App.js
@@ -12,20 +12,73 @@ class App extends Component {
   changeVolume(value) {
     // this.pizzicato.volume = value / 100;
   }
+  handleClick () {
+    console.log("działam");
+    //zmiana koloru
+  }
   render() {
     return (
       <div>
-        {/* <div className="player"> */}
+          {/* <div className="player"> */}
           <PlayAudio id="player" url={'http://www.noiseaddicts.com/samples_1w72b820/4186.mp3'}/>    
-          <Slider className="volume" vertical={true} defaultValue={100} onChange={this.changeVolume.bind(this)}>
-            <p>Volume</p>
-          </Slider>
-        {/* <Player src="https://www.soundeffectsplus.com/uploads/prod_audio/39398746_airplane-fly-over-large-airliner-03.mp3"/> */}
-          <img src={music} className="music"/>
-          <Player/>
+          <div className='row secondSection'>
+            <div className="col-5" >    
+                {/* <Player src="https://www.soundeffectsplus.com/uploads/prod_audio/39398746_airplane-fly-over-large-airliner-03.mp3"/> */}
+              <img src={music} className="music"/>
+            </div>
+             
+            <div className="col-5" >    
+              {/* <Player src="https://www.soundeffectsplus.com/uploads/prod_audio/39398746_airplane-fly-over-large-airliner-03.mp3"/> */}
+              <img src={music} className="music"/>
+            </div>
+            <div className="col-1">
+              <Slider className="volume" vertical={true} defaultValue={100} onChange={this.changeVolume.bind(this)}>
+                  <p>Gain</p>
+              </Slider>
+            </div>
+          </div>
+          <div className='row thirdSection'>
+            <div className="col-1">
+              <Slider className="volume" vertical={true} defaultValue={100} onChange={this.changeVolume.bind(this)}>
+                <p>Vol</p>
+              </Slider>
+            </div> 
+            <div className="col-4" >    
+            </div>
+            <div className="col-1">
+              <Slider className="volume" vertical={true} defaultValue={100} onChange={this.changeVolume.bind(this)}>
+                <p>Vol</p>
+              </Slider>
+            </div> 
+            <div className="col-4" >   
+            </div>
+          </div>
+
+          <div className='row fourthSection'>
+            <div className="col-2">
+                <button className="effects" onClick={this.handleClick}>Delay</button>
+            </div> 
+            <div className="col-2">
+                <button className="effects">Quad fuzz</button>
+            </div>
+            <div className="col-2">
+                <button className="effects">Flanger</button>
+            </div>
+            <div className="col-2">
+                <button className="effects">Distortion</button>
+            </div>
+            <div className="col-2">
+                <button className="effects">Reverb</button>
+            </div>
+            <div className="col-2">
+                <button className="effects"></button>
+            </div>
+          </div>
+
+          
         
       </div>
-      
+      // <Player/>
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
